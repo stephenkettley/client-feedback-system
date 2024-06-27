00bae8d6b50b5819c01a117cfd770f65bf09bb5b
Stephen Kettley <kettleystephen@gmail.com>
2024-06-27 15:42:34 +0200

    implement google authentication

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const googleKeys = require("./config/keys");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ success: false });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleKeys.googleClientID,
+      clientSecret: googleKeys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token ", accessToken);
+      console.log("refresh token ", refreshToken);
+      console.log("profile ", profile);
+      console.log("done ", done);
+    }
+  )
+);
 
-app.listen(5000);
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
